115585092c8dbb0130368d8cef1b7d7caf4245d7
Redace10 <lee_myung_jae@hotmail.com>
2018-10-12 22:14:24 -0400

    added loop to request for images

--- code/search_bing_api.py
+++ code/search_bing_api.py
@@ -4,12 +4,51 @@ import argparse
 import requests
 import cv2
 import os
-print("aaa")
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-q", "--query", required=True,
-	help="search query to search Bing Image API for")
-ap.add_argument("-o", "--output", required=True,
-	help="path to output directory of images")
+ap.add_argument("-q", "--query", required=True, help="search query to search Bing Image API for")
+ap.add_argument("-o", "--output", required=True, help="path to output directory of images")
 args = vars(ap.parse_args())
+
+API_KEY = "f03d4add454f42e7ad180becfbb497d4"
+MAX_RESULTS = 250
+GROUP_SIZE = 50
+ 
+# set the endpoint API URL
+URL = "https://api.cognitive.microsoft.com/bing/v7.0/images/search"
+
+EXCEPTIONS = set([IOError, FileNotFoundError,
+	exceptions.RequestException, exceptions.HTTPError,
+	exceptions.ConnectionError, exceptions.Timeout])
+
+# store the search term in a convenience variable then set the
+# headers and search parameters
+term = args["query"]
+headers = {"Ocp-Apim-Subscription-Key" : API_KEY}
+params = {"q": term, "offset": 0, "count": GROUP_SIZE}
+ 
+# make the search
+print("[INFO] searching Bing API for '{}'".format(term))
+search = requests.get(URL, headers=headers, params=params)
+search.raise_for_status()
+ 
+# grab the results from the search, including the total number of
+# estimated results returned by the Bing API
+results = search.json()
+estNumResults = min(results["totalEstimatedMatches"], MAX_RESULTS)
+print("[INFO] {} total results for '{}'".format(estNumResults, term))
+
+total = 0
+# loop over the estimated number of results in `GROUP_SIZE` groups
+for offset in range(0, estNumResults, GROUP_SIZE):
+	# update the search parameters using the current offset, then
+	# make the request to fetch the results
+	print("[INFO] making request for group {}-{} of {}...".format(
+		offset, offset + GROUP_SIZE, estNumResults))
+	params["offset"] = offset
+	search = requests.get(URL, headers=headers, params=params)
+	search.raise_for_status()
+	results = search.json()
+	print("[INFO] saving images for group {}-{} of {}...".format(
+		offset, offset + GROUP_SIZE, estNumResults))
