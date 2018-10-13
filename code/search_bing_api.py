6dc2ff810987189caf6d335bc66c512882ccdcce
Redace10 <lee_myung_jae@hotmail.com>
2018-10-13 18:30:13 -0400

    made code to scrape specified images using bing api

--- code/search_bing_api.py
+++ code/search_bing_api.py
@@ -11,12 +11,12 @@ ap.add_argument("-q", "--query", required=True, help="search query to search Bin
 ap.add_argument("-o", "--output", required=True, help="path to output directory of images")
 args = vars(ap.parse_args())
 
-API_KEY = "f03d4add454f42e7ad180becfbb497d4"
+API_KEY = "e03905dd934649659931be63098e10a3"
 MAX_RESULTS = 250
 GROUP_SIZE = 50
  
 # set the endpoint API URL
-URL = "https://api.cognitive.microsoft.com/bing/v7.0/images/search"
+URL = "https://api.cognitive.microsoft.com/bing/v5.0/images/search"
 
 EXCEPTIONS = set([IOError, FileNotFoundError,
 	exceptions.RequestException, exceptions.HTTPError,
@@ -44,11 +44,39 @@ total = 0
 for offset in range(0, estNumResults, GROUP_SIZE):
 	# update the search parameters using the current offset, then
 	# make the request to fetch the results
-	print("[INFO] making request for group {}-{} of {}...".format(
-		offset, offset + GROUP_SIZE, estNumResults))
+	print("[INFO] making request for group {}-{} of {}...".format(offset, offset + GROUP_SIZE, estNumResults))
 	params["offset"] = offset
 	search = requests.get(URL, headers=headers, params=params)
 	search.raise_for_status()
 	results = search.json()
-	print("[INFO] saving images for group {}-{} of {}...".format(
-		offset, offset + GROUP_SIZE, estNumResults))
+	print("[INFO] saving images for group {}-{} of {}...".format(offset, offset + GROUP_SIZE, estNumResults))
+
+	# save images in the current batch
+	for v in results["value"]:
+		try:
+			print("[INFO] fetching: {}".format(v["contentUrl"]))
+			req = requests.get(v["contentUrl"], timeout=30)
+
+			# make the path to the image
+			ext = v["contentUrl"][v["contentUrl"].rfind("."):]
+			path = os.path.sep.join([args["output"], "{}{}".format(str(total).zfill(8), ext)])
+
+			# write the image to disk
+			f = open(path, "wb")
+			f.write(req.content)
+			f.close()
+		
+		except Exception as e:
+			if type(e) in EXCEPTIONS:
+				print("[INFO] skipping: {}".format(v["contentUrl"]))
+				continue
+		
+		# check if image can be loaded by OpenCV
+		image = cv2.imread(path)
+		if image is None:
+			print("[INFO] deleting: {}".format(path))
+			os.remove(path)
+			continue
+
+		total += 1
+
