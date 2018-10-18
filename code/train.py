4404c63faa776213b6d67d75c0a93e2ec327c7c6
Redace10 <lee_myung_jae@hotmail.com>
2018-10-17 20:44:01 -0400

    Added train.py

---
+++ code/train.py
@@ -0,0 +1,18 @@
+import matplotlib
+matplotlib.use("Agg")
+
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import Adam
+from keras.preprocessing.image import img_to_array
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from pyimagesearch.smallervggnet import SmallerVGGNet
+import matplotlib.pyplot as plt
+from imutils import paths
+import numpy as np
+import argparse
+import random
+import pickle
+import cv2
+import os
+
