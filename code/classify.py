5711d2f9fdf60a31f4af189956455c7b682e1f73
Redace10 <lee_myung_jae@hotmail.com>
2018-10-18 22:11:16 -0400

    finished classify.py

--- code/classify.py
+++ code/classify.py
@@ -23,3 +23,31 @@ image = cv2.resize(image, (96, 96))
 image = image.astype("float") / 255.0
 image = img_to_array(image)
 image = np.expand_dims(image, axis=0)
+
+# load the trained convolutional neural network and the label binarizer
+print("[INFO] loading network...")
+model = load_model(args["model"])
+lb = pickle.loads(open(args["labelbin"], "rb").read())
+ 
+# classify the input image
+print("[INFO] classifying image...")
+proba = model.predict(image)[0]
+idx = np.argmax(proba)
+label = lb.classes_[idx]
+
+# we'll mark our prediction as "correct" of the input image filename
+# contains the predicted label text (obviously this makes the
+# assumption that you have named your testing image files this way)
+filename = args["image"][args["image"].rfind(os.path.sep) + 1:]
+correct = "correct" if filename.rfind(label) != -1 else "incorrect"
+ 
+# build the label and draw the label on the image
+label = "{}: {:.2f}% ({})".format(label, proba[idx] * 100, correct)
+output = imutils.resize(output, width=400)
+cv2.putText(output, label, (10, 25),  cv2.FONT_HERSHEY_SIMPLEX,
+	0.7, (0, 255, 0), 2)
+ 
+# show the output image
+print("[INFO] {}".format(label))
+cv2.imshow("Output", output)
+cv2.waitKey(0)
