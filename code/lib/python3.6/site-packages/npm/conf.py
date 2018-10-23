414d8224d79f3ea049a1e570e2fcd7c7273803e0
Redace10 <lee_myung_jae@hotmail.com>
2018-10-22 21:41:13 -0400

    removed unnecessary files

--- code/lib/python3.6/site-packages/npm/conf.py
+++
@@ -1,9 +0,0 @@
-from optional_django.conf import Conf
-
-settings = Conf('NPM', {
-    'PATH': 'npm',
-    'VERSION_COMMAND': '--version',
-    'VERSION_FILTER': lambda version: tuple(map(int, version.split('.'))),
-    'INSTALL_COMMAND': 'install',
-    'INSTALL_PATH_TO_PYTHON': None,
-})
